e172421ce0f53db9e5229152cb19a3903a463780
Travis Reed <tdreed@gmail.com>
2025-12-08 22:21:56 -0800

    Reorganize architecture module imports and improve formatting in x86_64 trap handling

--- boot/src/arch/mod.rs
+++ boot/src/arch/mod.rs
@@ -1,7 +1,11 @@
-pub mod x86_64;
+#[cfg(target_arch = "aarch64")]
 pub mod aarch64;
-pub mod riscv64;
+#[cfg(target_arch = "loongarch64")]
 pub mod loongarch64;
+#[cfg(target_arch = "riscv64")]
+pub mod riscv64;
+#[cfg(target_arch = "x86_64")]
+pub mod x86_64;
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]

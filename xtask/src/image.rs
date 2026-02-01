e761326efbb057322a2d8949f63225949bd7ee35
Travis Reed <tdreed@gmail.com>
2026-01-31 19:22:46 -0800

    Fix some rustflags

--- xtask/src/image.rs
+++ xtask/src/image.rs
@@ -512,7 +512,7 @@ fn build_userspace_app_with_features(
 
     let mut cmd = cmd!(
         sh,
-        "cargo build --target {target} --profile {profile} -p {name} -Z build-std=core,alloc -Z build-std-features=compiler-builtins-mem -Z json-target-spec"
+        "cargo -Zjson-target-spec -Z build-std=core,alloc -Z build-std-features=compiler-builtins-mem build --target {target} --profile {profile} -p {name}"
     )
     .env("RUSTFLAGS", "-Awarnings");
 

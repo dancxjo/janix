57b6d7490c181343ecaab1b526e615b988b41b60
Travis Reed <tdreed@gmail.com>
2026-02-06 12:23:50 -0800

    feat: update default wallpaper to linen.bmp and add it to allowed assets

--- userspace/bloom/src/painter_resources.rs
+++ userspace/bloom/src/painter_resources.rs
@@ -29,8 +29,8 @@ pub extern "C" fn asset_watcher_entry() -> ! {
 
 pub extern "C" fn wallpaper_loader_entry() -> ! {
     stem::sleep_ms(100);
-    debug!("[bloom] wallpaper loader: loading leather.bmp");
-    ASSETS.enqueue_wallpaper_load("leather.bmp");
+    debug!("[bloom] wallpaper loader: loading linen.bmp");
+    ASSETS.enqueue_wallpaper_load("linen.bmp");
     loop {
         stem::syscall::sleep_ms(10000);
     }
--- xtask/src/image.rs
+++ xtask/src/image.rs
@@ -202,6 +202,7 @@ fn generate_limine_config(
         let allowed = clean_path.ends_with("NotoSans-Regular.ttf")
             || clean_path.ends_with("future/default.svg")
             || clean_path.ends_with("wallpapers/leather.bmp")
+            || clean_path.ends_with("wallpapers/linen.bmp")
             || clean_path.ends_with("themes/genie_circles.wasm");
 
         if allowed {
